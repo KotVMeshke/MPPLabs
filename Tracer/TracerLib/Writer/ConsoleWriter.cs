36bfcf2c9e9dbf9546f79258a70b2ff1a7f388b5
KotVMeshke <dimon2003mail.ru37@gmail.com>
2024-03-25 23:31:42 +0300

    Added writer into console and files

---
+++ Tracer/TracerLib/Writer/ConsoleWriter.cs
@@ -0,0 +1,17 @@
+﻿using MPP_LABA1_Console.Writer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPP_LABA1_Console.Writer
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void Write(params string[] text)
+        {
+            Console.WriteLine(text[0]);
+        }
+    }
+}
---
+++ Tracer/TracerLib/Writer/FileWriter.cs
@@ -0,0 +1,24 @@
+﻿using MPP_LABA1_Console.Writer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPP_LABA1_Console.Writer
+{
+    public class FileWriter : IWriter
+    {
+        public void Write(params string[] text)
+        {
+            if (text.Length == 2)
+            {
+                using var file = new StreamWriter(text[1], false);
+                file.Write(text[0]);
+            }else
+            {
+                throw new Exception("Incorrect parametrs count");
+            }
+        }
+    }
+}
